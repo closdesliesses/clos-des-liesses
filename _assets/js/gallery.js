dce0c8a8096fe836554515c00dd5bf2df117096e
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2018-07-26 00:30:09 +0200

    add tabs for gallery

--- _assets/js/gallery.js
+++ _assets/js/gallery.js
@@ -1,30 +1,14 @@
 $(document).ready(function () {
 
-  // Swiper
-  var galleryAll = new Swiper('#swiper-container--all', {
-    spaceBetween: 10,
-    centeredSlides: true,
-    loop: true,
-    lazyLoading: true,
-    autoplay: false,
-    speed: 600,
-    slidesPerView: 'auto',
-    spaceBetween: 75,
-    observer: true,
-    observeParents: true,
-    initialSlide: 0,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // var domainPictures = 28
 
+  // for(var imgIndex = 1; imgIndex < domainPictures; imgIndex++) {
+  //   debugger;
+  //   $('#swiper-container--domain').find('swiper-wrapper').innerHTML += '<div class="swiper-slide" style="background-image:url(../assets/images/domaine/domaine-' + imgIndex + '.jpg)"></div>'
+  // }
+
+  // Swiper
   var galleryDomain = new Swiper('#swiper-container--domain', {
-    spaceBetween: 10,
     centeredSlides: true,
     loop: true,
     lazyLoading: true,
@@ -45,7 +29,6 @@ $(document).ready(function () {
   });
 
   var galleryCeremony = new Swiper('#swiper-container--ceremony', {
-    spaceBetween: 10,
     centeredSlides: true,
     loop: true,
     lazyLoading: true,
@@ -65,49 +48,64 @@ $(document).ready(function () {
     },
   });
 
-  $("#swiper-container--domain, #swiper-container--ceremony").hide();
-  $('.navigation__tab[data-selector="all"]').addClass('navigation__tab--selected');
-  if (location.hash == '#all') {
-    $("#swiper-container--all").show();
-    $("#swiper-container--domain, #swiper-container--ceremony").hide();
-    $('.navigation__tab[data-selector="all"]').addClass('navigation__tab--selected');
-  } else if (location.hash == '#domaine') {
+  var galleryCocktail = new Swiper('#swiper-container--cocktail', {
+    centeredSlides: true,
+    loop: true,
+    lazyLoading: true,
+    autoplay: false,
+    speed: 600,
+    slidesPerView: 'auto',
+    spaceBetween: 75,
+    observer: true,
+    observeParents: true,
+    initialSlide: 0,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $("#swiper-container--cocktail, #swiper-container--ceremony").hide();
+  $('.navigation__tab[data-selector="domain"]').addClass('navigation__tab--selected');
+  if (location.hash == '#domaine') {
     $("#swiper-container--domain").show();
-    $("#swiper-container--all, #swiper-container--ceremony").hide();
+    $("#swiper-container--cocktail, #swiper-container--ceremony").hide();
     $('.navigation__tab').removeClass('navigation__tab--selected');
     $('.navigation__tab[data-selector="domain"]').addClass('navigation__tab--selected');
   } else if (location.hash == '#ceremony') {
     $("#swiper-container--ceremony").show();
-    $("#swiper-container--all, #swiper-container--domain").hide();
+    $("#swiper-container--cocktail, #swiper-container--domain").hide();
     $('.navigation__tab').removeClass('navigation__tab--selected');
     $('.navigation__tab[data-selector="ceremony"]').addClass('navigation__tab--selected');
+  } else if (location.hash == '#cocktail') {
+    $("#swiper-container--cocktail").show();
+    $("#swiper-container--domain, #swiper-container--ceremony").hide();
+    $('.navigation__tab').removeClass('navigation__tab--selected');
+    $('.navigation__tab[data-selector="cocktail"]').addClass('navigation__tab--selected');
   }
 
   $(".navigation__tab").click(function() {
-    galleryDomain.update();
-    galleryCeremony.update();
-    galleryAll.update();
-    galleryDomain.navigation.update();
-    galleryCeremony.navigation.update();
-    galleryAll.navigation.update();
+    
     var blockName = $(this).data('selector');
     $('.navigation__tab').removeClass('navigation__tab--selected');
     $(this).addClass('navigation__tab--selected');
     if(blockName == 'domain') {
       $("#swiper-container--domain").show();
-      $("#swiper-container--all, #swiper-container--ceremony").hide();
+      $("#swiper-container--cocktail, #swiper-container--ceremony").hide();
       window.location.hash = 'domaine';
     } else if(blockName == 'ceremony') {
       $("#swiper-container--ceremony").show();
-      $("#swiper-container--all, #swiper-container--domain").hide();
+      $("#swiper-container--cocktail, #swiper-container--domain").hide();
       window.location.hash = 'ceremony';
     } else {
-      $("#swiper-container--all").show();
+      $("#swiper-container--cocktail").show();
       $("#swiper-container--domain, #swiper-container--ceremony").hide();
-      window.location.hash = 'all';
+      window.location.hash = 'cocktail';
     }
-  });
-
-  
+  });  
 
 });
