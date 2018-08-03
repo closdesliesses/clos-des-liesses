f8890fb026283b3827fd021458dd9142bf68cd84
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2018-08-03 20:19:27 +0200

    add scroll indicator on event-pro page

--- _assets/js/pro-event.js
+++ _assets/js/pro-event.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $(window).resize(function() {
     $('.pro-event__background-intro').height($(window).height());
 
-    $(".scroll-indicator-bullets--pro-event").css({"top": $(window).height() + 50 + "px", "height": "240px"});
+    $(".scroll-indicator-bullets--pro-event").css({"top": $(window).height() + 50 + "px", "height": "120px"});
   });
 
   $(window).trigger('resize');
@@ -28,41 +28,28 @@ $(document).ready(function(){
     };
 
     // Color current bullet
-    var proEventBlock1 = $('#pro-event-block-1').offset().top - 100,
-        proEventBlock2 = $('#pro-event-block-2').offset().top - 100,
-        proEventBlock3 = $('#pro-event-block-3').offset().top - 100,
-        proEventBlock4 = $('#pro-event-block-4').offset().top - 100,
-        proEventBlock5 = $('#pro-event-block-5').offset().top - 100,
-        proEventBlock6 = $('#pro-event-block-6').offset().top - 100,
-        proEventBlock7 = $('#pro-event-block-7').offset().top - 100,
-        proEventBlock8 = $('#pro-event-block-8').offset().top - 100,
-        proEventBlock9 = $('#pro-event-block-9').offset().top - 100,
+    var proEventBlock1 = $('#pro-event-block-centered').offset().top - 100,
+        proEventBlock2 = $('#pro-event-block-1').offset().top - 100,
+        proEventBlock3 = $('#pro-event-block-2').offset().top - 100,
+        proEventBlock4 = $('#pro-event-block-3').offset().top - 100,
+        proEventBlock5 = $('#pro-event-block-4').offset().top - 100,
         footer = $('footer').offset().top - 100;
 
     var scrollPos = $(document).scrollTop();
 
-    $('.bullet-pro-event-1, .bullet-pro-event-2, .bullet-pro-event-3, .bullet-pro-event-4, .bullet-pro-event-5, .bullet-pro-event-6, .bullet-pro-event-7, .bullet-pro-event-8, .bullet-pro-event-9').removeClass('current');
+    $('.bullet-pro-event-centered, .bullet-pro-event-1, .bullet-pro-event-2, .bullet-pro-event-3, .bullet-pro-event-4').removeClass('current');
 
     if (scrollPos >= proEventBlock1 && scrollPos < proEventBlock2) {
-      $('.bullet-pro-event-1').addClass('current');
+      $('.bullet-pro-event-centered').addClass('current');
     } else if (scrollPos >= proEventBlock2 && scrollPos < proEventBlock3) {
-      $('.bullet-pro-event-2').addClass('current');
+      $('.bullet-pro-event-1').addClass('current');
     } else if (scrollPos >= proEventBlock3 && scrollPos < proEventBlock4) {
-      $('.bullet-pro-event-3').addClass('current');
+      $('.bullet-pro-event-2').addClass('current');
     } else if (scrollPos >= proEventBlock4 && scrollPos < proEventBlock5) {
+      $('.bullet-pro-event-3').addClass('current');
+    } else if (scrollPos >= proEventBlock5 && scrollPos < footer) {
       $('.bullet-pro-event-4').addClass('current');
-    } else if (scrollPos >= proEventBlock5 && scrollPos < proEventBlock6) {
-      $('.bullet-pro-event-5').addClass('current');
-    } else if (scrollPos >= proEventBlock6 && scrollPos < proEventBlock7) {
-      $('.bullet-pro-event-6').addClass('current');
-    } else if (scrollPos >= proEventBlock7 && scrollPos < proEventBlock8) {
-      $('.bullet-pro-event-7').addClass('current');
-    } else if (scrollPos >= proEventBlock8 && scrollPos < proEventBlock9) {
-      $('.bullet-pro-event-8').addClass('current');
-    } else if (scrollPos >= proEventBlock9 && scrollPos < footer) {
-      $('.bullet-pro-event-9').addClass('current');
     }
-
   });
 
 });
